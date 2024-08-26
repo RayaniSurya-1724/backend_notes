c3116d7de7fc70d2f27418db28b53306bf00f460
rayanisurya <rayanisurya1724@gmail.com>
2024-08-26 13:46:20 +0530

    rename

--- script1.js
+++ script1.js
@@ -8,3 +8,12 @@ fs.appendFile("hi.txt","he is a student",function(err){
         console.log("done");
     }
 })
+
+fs.rename("hi.txt","hello.txt",function(err){
+    if(err){
+        console.error(err);
+    }
+    else{
+        console.log("done");
+    }
+})
