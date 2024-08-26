2abe62ad06a3025839f9eeb193a4096af0112991
rayanisurya <rayanisurya1724@gmail.com>
2024-08-26 13:40:19 +0530

    appendfile

--- script1.js
+++ script1.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.writeFile("hi.txt","this is surya",function(err){
+fs.appendFile("hi.txt","he is a student",function(err){
     if(err){
         console.error(err);
     }
